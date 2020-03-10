a6a9ae819a1852e4a3e9a03934bbf5db655e32fd
daniel,kim <60847008+nalaolla@users.noreply.github.com>
2020-03-11 01:26:21 +0900

    0.0.3

--- src/main/java/com/nalaolla/awsweb/controller/DefaultController.java
+++ src/main/java/com/nalaolla/awsweb/controller/DefaultController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class DefaultController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String main(Model model) {
 
         model.addAttribute("title", "awsweb title");
