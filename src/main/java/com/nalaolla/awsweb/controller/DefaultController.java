5f7cfa9a1f56da79e790578b1081948b8b89470d
daniel,kim <60847008+nalaolla@users.noreply.github.com>
2020-03-11 01:54:24 +0900

    0.0.7

--- src/main/java/com/nalaolla/awsweb/controller/DefaultController.java
+++ src/main/java/com/nalaolla/awsweb/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package com.nalaolla.awsweb.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
 @RequestMapping("/")
 public class DefaultController {
 
