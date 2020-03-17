24a06cafcf9a1e1764203cd4f7ae9b4aa6bb1801
nalaolla <palaoh@gmail.com>
2020-03-17 17:06:41 +0900

    0.1.15

--- src/main/java/com/nalaolla/awsweb/controller/DefaultController.java
+++ src/main/java/com/nalaolla/awsweb/controller/DefaultController.java
@@ -33,8 +33,8 @@ public class DefaultController {
     @GetMapping("")
     public String postError(Model model) {
 
-        model.addAttribute("title", "awsweb error");
-        model.addAttribute("contents", "awsweb error");
+        model.addAttribute("title", "awsweb default");
+        model.addAttribute("contents", "awsweb default");
 
         return "default";
     }
